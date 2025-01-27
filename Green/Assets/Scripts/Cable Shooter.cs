aebefa43bd3462a494a99f362cae12545ae96e48
AlCh440 <73243813+AlCh440@users.noreply.github.com>
2025-01-27 19:43:18 +0100

    fixing update

--- Green/Assets/Scripts/Cable Shooter.cs
+++ Green/Assets/Scripts/Cable Shooter.cs
@@ -13,7 +13,7 @@ public class CableShooter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //debug Log
     }
 
     // Update is called once per frame
